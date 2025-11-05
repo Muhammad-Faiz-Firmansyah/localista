c863c0694567ce86cb825a092de1870353ea8cde
MuhammadFaiqAudah <lagilaperrr@gmail.com>
2025-11-05 15:56:00 +0700

    Add Parallax image as background to footer component

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,10 +1,11 @@
 import {} from 'react';
+import Parallax from "./assets/Parallax.png";
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-100 md:py-50 mt-10 border-t border-slate-200 py-20">
+    <footer className="md:py-50 mt-10 border-t border-slate-200 py-20" style={{ backgroundImage: `url(${Parallax})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="mx-auto max-w-7xl px-4 md:px-6 text-right text-xl font-montserrat font-bold text-black">
-        Localitsa
+        Localista
       </div>
     </footer>
   );
